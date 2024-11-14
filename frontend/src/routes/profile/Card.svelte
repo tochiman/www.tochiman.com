da2fd9cf6364b26bc3bb803d5d509c0342107494
tochiman <developer@tochiman.com>
2024-11-14 14:43:07 +0000

    Update パソコン向けには完成。スマホサイズには未対応

---
+++ frontend/src/routes/profile/Card.svelte
@@ -0,0 +1,73 @@
+<script lang="ts">
+    import { 
+        Button,
+        GradientButton,
+        Card,
+        Img,
+        P,
+    } from 'flowbite-svelte';
+    import { 
+        ArrowRightOutline, 
+    } from 'flowbite-svelte-icons';
+    export let filter;
+</script>
+{#if filter === "sns" || filter === "all"}
+<Card class="w-full">
+    <Img src="discord.svg" class="w-8 h-8 mb-2" />
+        <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Discord</h5>
+    <P class="mb-2 font-normal text-gray-500 dark:text-gray-400">tochiman.com</P>
+    <Button size="md" outline class="w-fit" href="https://discord.com/channels/@me" style="color:white; background-color:#5865F2;">
+        Friend <ArrowRightOutline class="w-6 h-6 ms-2 text-white" />
+    </Button>
+</Card>
+{/if}
+{#if filter === "tech" || filter === "all"}
+<Card class="w-full">
+    <Img src="github-mark.svg" class="w-8 h-8 mb-2" />
+        <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Github</h5>
+    <P class="mb-2 font-normal text-gray-500 dark:text-gray-400">tochiman</P>
+    <Button size="md" class="w-fit" href="https://github.com/tochiman/" color="alternative">
+        Follow <ArrowRightOutline class="w-6 h-6 ms-2 dark:text-white" />
+    </Button>
+</Card>
+{/if}
+{#if filter === "sns" || filter === "all"}
+<Card class="w-full">
+    <Img src="x-black.svg" class="w-8 h-8 mb-2" />
+        <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">X</h5>
+    <P class="mb-2 font-normal text-gray-500 dark:text-gray-400">@NTR_tochiman</P>
+    <Button size="md" outline class="w-fit" href="https://x.com/NTR_tochiman" color="alternative">
+        Follow <ArrowRightOutline class="w-6 h-6 ms-2 dark:text-white" />
+    </Button>
+</Card>
+{/if}
+{#if filter === "tech" || filter === "all"}
+<Card class="w-full">
+    <Img src="zenn.svg" class="w-8 h-8 mb-2" />
+        <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Zenn</h5>
+    <P class="mb-2 font-normal text-gray-500 dark:text-gray-400">tochiman</P>
+    <Button size="md" class="w-fit" href="https://zenn.dev/tochiman" color="blue">
+        Follow <ArrowRightOutline class="w-6 h-6 ms-2 text-white" />
+    </Button>
+</Card>
+{/if}
+{#if filter === "sns" || filter === "all"}
+<Card class="w-full">
+    <Img src="Instagram.svg" class="w-8 h-8 mb-2" />
+        <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Instagram</h5>
+    <P class="mb-2 font-normal text-gray-500 dark:text-gray-400">ntr_tochiman</P>
+    <GradientButton size="md" class="w-fit" href="https://www.instagram.com/ntr_tochiman/" color="pinkToOrange">
+        Follow <ArrowRightOutline class="w-6 h-6 ms-2 text-white" />
+    </GradientButton>
+</Card>
+{/if}
+{#if filter === "other" || filter === "all"}
+<Card  class="w-full">
+    <Img src="mail.png" class="w-9 h-9 mb-2" />
+        <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">E-Mail</h5>
+    <P class="mb-2 font-normal text-gray-500 dark:text-gray-400" whitespace="normal">developer@tochiman.com</P>
+    <Button size="md" class="w-fit" href="mailto:developer@tochiman.com">
+        Mail <ArrowRightOutline class="w-6 h-6 ms-2 text-white" />
+    </Button>
+</Card>
+{/if}
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -3,13 +3,62 @@ import containerQueries from '@tailwindcss/container-queries';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
+import flowbitePlugin from 'flowbite/plugin'
 
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'],
+
+  darkMode: 'selector',
 
   theme: {
-    extend: {}
+    extend: {
+      animation: {
+        "flip-out-ver-left": "flip-out-ver-left 0.4s cubic-bezier(0.550, 0.085, 0.680, 0.530)   both"
+      },
+      keyframes: {
+        "flip-out-ver-left": {
+            "0%": {
+                transform: "rotateY(0)",
+                opacity: "1"
+            },
+            to: {
+                transform: "rotateY(-70deg)",
+                opacity: "0"
+            }
+        }
+      },
+      borderWidth: {
+        DEFAULT: '1px',
+        '0': '0',
+        '2': '2px',
+        '3': '3px',
+        '4': '4px',
+        '6': '6px',
+        '8': '8px',
+        '10': '10px',
+        '12': '12px',
+        '14': '14px',
+      },
+      // width: {
+      //   ''
+      // },
+      colors: {
+        // flowbite-svelte
+        primary: {
+          50: '#FFF5F2',
+          100: '#FFF1EE',
+          200: '#FFE4DE',
+          300: '#FFD5CC',
+          400: '#FFBCAD',
+          500: '#FE795D',
+          600: '#EF562F',
+          700: '#EB4F27',
+          800: '#CC4522',
+          900: '#A5371B'
+        }
+      }
+    }
   },
 
-  plugins: [typography, forms, containerQueries, aspectRatio]
+  plugins: [flowbitePlugin, typography, forms, containerQueries, aspectRatio]
 } satisfies Config;
